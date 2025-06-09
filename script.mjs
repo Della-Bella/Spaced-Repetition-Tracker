4241d985d2f6eac654be2f07fc59187b47642ff0
eyuell21 <abrahameyuel4@gmail.com>
2025-06-09 22:19:32 +0100

    Fixed typos

--- script.mjs
+++ script.mjs
@@ -67,7 +67,7 @@ function calculateRevisionDates (startDateString, topicName) {
     const mm = String (date.getMonth()+1).padStart(2, '0');
     const dd = String (date.getDate()).padStart(2, '0');
     revisions.push({
-      topic: topicName;
+      topic: topicName,
       revisionDate: `${yyyy}-${mm}-${dd}`
     });
   };
