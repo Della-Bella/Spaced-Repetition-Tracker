4c9b71a385181104087a7aa7b3b1bb52e162e6bb
Fradoka <franklindkamela@gmail.com>
2025-06-10 21:41:35 +0100

    filtering and sorting dates

--- script.mjs
+++ script.mjs
@@ -114,12 +114,19 @@ userDropdown.addEventListener('change', (event) => {
     return;
   }
 
+  const today = new Date();
+  today.setHours(0,0,0,0);
+
+  const items = agendaItems
+    .filter(item => new Date(item.revisionDate) >= today)
+    .sort((a,b) => new Date(a.revisionDate) - new Date(b.revisionDate));
+
   const ul = document.createElement('ul');
-  agendaItems.forEach(item => {
+  items.forEach(item => {
     const li = document.createElement('li');
     li.textContent = `${item.topic} - ${item.revisionDate}`;
     ul.appendChild(li);
-  })
+  });
   agendaContainer.appendChild(ul);
 })
 
